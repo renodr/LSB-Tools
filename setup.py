43a98d8d1c1773a96ce7aa0930437a5a4d206379
DJ Lucas <dj@lucasit.com>
2019-08-10 16:58:46 -0500

    setup.py: complete override of install for --root

--- setup.py
+++ setup.py
@@ -1,24 +1,38 @@
 from setuptools import setup
 from setuptools.command.install import install
-import site, os
+import os, re, site, sys
+
 
 class OSInstall(install):
     """Allow OS installation"""
     def run(self):
+        foundroot = 0
         install.run(self)
-        lsbdir = "/usr/lib/lsb"
-        bindir = "/usr/bin"
-        pkgdir = site.getsitepackages()[0]
-        pkgdir = os.path.join(pkgdir, "lsbtools")
-        iid = os.path.join(pkgdir, "install_initd.py")
-        did = os.path.join(lsbdir, "install_initd")
+        for arg in sys.argv:
+            if re.match("--root=.*", arg):
+                rootdir = arg.split("=")[1]
+                foundroot = 1
+        if foundroot == 0:
+            rootdir = ""
+        lsbdir = rootdir + "/usr/lib/lsb"
+        #bindir = rootdir + "/usr/bin"
+        sitepkgdir = site.getsitepackages()[0]
+        pkgdir = rootdir + sitepkgdir + "/lsbtools"
+        if not os.path.exists(lsbdir):
+            os.mkdir(lsbdir)
+        #if not os.path.exists(bindir):
+        #    os.mkdir(bindir)
+        iid = pkgdir + "/install_initd.py"
+        riid = sitepkgdir + "/lsbtools" + "/install_initd.py"
+        did = lsbdir + "/install_initd"
         dtid = did + '.temp'
-        ird = os.path.join(pkgdir, "remove_initd.py")
-        drd = os.path.join(lsbdir, "remove_initd")
+        ird = pkgdir + "/remove_initd.py"
+        rird = sitepkgdir + "/lsbtools" + "/remove_initd.py"
+        drd = lsbdir + "/remove_initd"
         dtrd = drd + '.temp'
-        os.symlink(iid, dtid)
+        os.symlink(riid, dtid)
         os.rename(dtid, did)
-        os.symlink(ird, dtrd)
+        os.symlink(rird, dtrd)
         os.rename(dtrd, drd)
         os.chmod(iid, 0o755)
         os.chmod(ird, 0o755)
